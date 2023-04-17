b0f88229c940303e53ba3c0e07627a91a1e7acc5
Staiko <Staiko@DESKTOP-NUVQ8TO>
2023-04-17 14:27:41 +0300

    Add project files.

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                long sumRow = 0;
+                for (int col = 0; col < cols; col++)
+                {
+                    sumRow += matrix[row, col];
+                    Console.Write("{0,10}" ,matrix[row, col] );
+                }
+                Console.WriteLine($"      {sumRow / (double)cols}");
+            }   }
+    }
+}
---
+++ Zadacha3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] elements = Console.ReadLine().Split(' ');
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = int.Parse(elements[col]);
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+            int[] columnMin = new int[cols];
+            for (int i = 0; i < columnMin.Length; i++)
+            {
+                columnMin[i] = int.MaxValue;
+            }
+            for (int col = 0; col < cols; col++)
+            {
+                for (int row = 0; row < rows; row++)
+                {
+                    if (matrix[row, col] < columnMin[col])
+                    {
+                        columnMin[col] = matrix[row, col];
+                    }
+                }
+            }
+            foreach (int min in columnMin)
+            {
+                Console.Write( min + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
